0b0bf6fd43d5d726e28b49e55181ea0b946bd131
Anmol Sethi <anmol@aubble.com>
2015-09-21 14:22:46 -0400

    refactoring

--- main.go
+++ main.go
@@ -80,20 +80,20 @@ func main() {
 	if _, _, err := syscall.Syscall6(syscall.SYS_IOCTL, os.Stdin.Fd(), syscall.TIOCGETA, uintptr(unsafe.Pointer(&old)), 0, 0, 0); err != 0 {
 		log.Fatalln("not a terminal, got:", err)
 	}
+	cleanup := func() {
+		// make cursor visible
+		os.Stdin.Write([]byte{27, 91, 51, 52, 104, 27, 91, 63, 50, 53, 104})
+		// set tty to normal
+		if _, _, err := syscall.Syscall6(syscall.SYS_IOCTL, os.Stdin.Fd(), syscall.TIOCSETA, uintptr(unsafe.Pointer(&old)), 0, 0, 0); err != 0 {
+			log.Fatal(err)
+		}
+	}
 	// capture signals
 	g.sigs = make(chan os.Signal)
 	signal.Notify(g.sigs, syscall.SIGINT, syscall.SIGTERM)
 	go func() {
 		<-g.sigs
-
-		// make cursor visible
-		os.Stdin.Write([]byte{27, 91, 51, 52, 104, 27, 91, 63, 50, 53, 104})
-
-		// set tty to normal
-		if _, _, err := syscall.Syscall6(syscall.SYS_IOCTL, os.Stdin.Fd(), syscall.TIOCSETA, uintptr(unsafe.Pointer(&old)), 0, 0, 0); err != 0 {
-			log.Fatal(err)
-		}
-
+		cleanup()
 		os.Exit(0)
 	}()
 	// set raw mode
@@ -102,7 +102,9 @@ func main() {
 	if _, _, err := syscall.Syscall6(syscall.SYS_IOCTL, os.Stdin.Fd(), syscall.TIOCSETA, uintptr(unsafe.Pointer(&raw)), 0, 0, 0); err != 0 {
 		log.Fatal(err)
 	}
+
 	g.setDimensions()
+
 	var maxInit uint16
 	if g.h < g.w {
 		maxInit = g.h/2 - 1
@@ -111,8 +113,8 @@ func main() {
 	}
 	if g.init > maxInit {
 		log.Println("init too big, max init size for this h/w is", maxInit)
-		g.sigs <- syscall.SIGTERM
-		g.sigs <- syscall.SIGTERM
+		cleanup()
+		os.Exit(0)
 	}
 
 	// start game
